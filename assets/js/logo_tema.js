f68163c2e5b12e8bbd4f0313c74c62376b22f3b8
javier montoya <montoya.javier.15@hotmail.com>
2024-04-08 18:17:28 -0600

    Cambio de logo entre temas Funcional

--- assets/js/logo_tema.js
+++ assets/js/logo_tema.js
@@ -1,12 +1,17 @@
-// Función para cambiar la imagen
 function cambiarImagen() {
     var imagen = document.getElementById("logo");
     // Verificar qué imagen está actualmente mostrándose
     if (imagen.src.endsWith("solt_w.png")) {
-        imagen.src = "../img/solt_b.png"; // Cambiar a la imagen de tema oscuro
+        imagen.src = "assets/img/solt_b.png"; // Cambiar a la imagen de tema oscuro
         imagen.alt = "Imagen en tema oscuro"; // Cambiar el texto alternativo si es necesario
+        imagen.classList.remove("home__blob-img"); // Remover la clase de tema claro
+        imagen.classList.add("home__blob"); // Agregar la clase de tema oscuro
+        
     } else {
-        imagen.src = "../img/solt_w.png"; // Volver a la imagen de tema claro
+        imagen.src = "assets/img/solt_w.png"; // Volver a la imagen de tema claro
         imagen.alt = "Imagen en tema claro"; // Cambiar el texto alternativo si es necesario
+        imagen.classList.remove("home__blob-img"); // Remover la clase de tema claro
+        imagen.classList.add("home__blob"); // Agregar la clase de tema oscuro
+        
     }
 }
